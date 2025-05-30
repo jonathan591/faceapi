3cea256260945122567156f57c2722c9e1809c26
jonathan cedeño <77215819+jonathan591@users.noreply.github.com>
2025-05-30 18:36:28 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+php artisan config:clear
+
+# Espera a que la base de datos esté disponible (opcional)
+# Puedes hacer un pequeño loop para esperar, o asumir que ya está lista
+
+php artisan migrate --force
+php artisan db:seed --force
+php artisan shield:super-admin
+php artisan storage:link
+
+# Finalmente arranca el servidor
+php artisan serve --host=0.0.0.0 --port=8000
