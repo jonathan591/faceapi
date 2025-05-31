3efc0877f7e0c8e51f986e9b74901f0a784ba2b4
jonathan cedeño <77215819+jonathan591@users.noreply.github.com>
2025-05-30 19:17:45 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ php artisan config:clear
 
 # Espera a que la base de datos esté disponible (opcional)
 # Puedes hacer un pequeño loop para esperar, o asumir que ya está lista
+# Asegura que el archivo SQLite existe
+touch /var/www/html/database/database.sqlite
 
+php artisan config:clear
 php artisan migrate --force
 php artisan db:seed --force
 
