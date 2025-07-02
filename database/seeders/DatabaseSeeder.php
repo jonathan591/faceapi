f3d1dec52ad1683ac554c699362e87fd5aceae31
jonathan cedeño <77215819+jonathan591@users.noreply.github.com>
2025-07-02 11:00:28 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-      
+  /**    
     \App\Models\User::factory()->create([
         'name' => 'admin',
         'email' => 'admin@gmail.com',
         'password' => bcrypt('12345'), // o Hash::make('12345')
     ]);
-   
+   */
     }
 }
